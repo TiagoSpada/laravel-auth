eddbc880c70f338344b6c926e5070d55cb60a9a0
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-23 15:14:12 +0200

    [upd] creazione colonne e popolate con faker

--- database/seeders/ProjectSeeder.php
+++ database/seeders/ProjectSeeder.php
@@ -1,20 +1,25 @@
 <?php
 
-namespace Database\Seeders\Admin;
+namespace Database\Seeders;
 
 
-use App\Http\Controllers\Controller;
-
+use App\Models\Project;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class ProjectSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        for ($i = 0; $i < 24; $i++) {
+            $project = new Project();
+            $project->title = $faker->sentence(3);
+            $project->description = $faker->text(100);
+            $project->save();
+        }
     }
 }
