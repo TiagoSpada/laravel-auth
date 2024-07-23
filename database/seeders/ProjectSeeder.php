2afb798ff89f50716e327499dfebfb1a188e3877
TiagoSpada <tiago.spada.ts@gmail.com>
2024-07-23 14:24:06 +0200

    [upd] importate view | create rotte e controller | creto model Project con relative migration, seeder, controller,

---
+++ database/seeders/ProjectSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders\Admin;
+
+
+use App\Http\Controllers\Controller;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProjectSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
